871dbff8b2b37c0c267131c3dc0e5b2677842e0e
7j2xgqhj <73765724+7j2xgqhj@users.noreply.github.com>
2023-05-30 11:29:10 +0900

    進捗

---
+++ envgui.py
@@ -0,0 +1,42 @@
+import tkinter as tk
+import numpy as np
+import environment
+WINDOW_WIDTH=WINDOW_HIGHT=CANVAS_WIDTH=CANVAS_HIGHT=400
+class EnvGUI(tk.Frame):
+    def __init__(self,env,master):
+        super().__init__(master)
+        self.master.geometry("400x400+200+200")
+        self.env=env
+        self.canvas= tk.Canvas(master, bg = "green"
+                               ,width=CANVAS_WIDTH,height=CANVAS_HIGHT)
+        for i in range(7):
+            self.canvas.create_line((i + 1) * (CANVAS_WIDTH / 8), 0, (i + 1) * (CANVAS_WIDTH / 8), CANVAS_HIGHT)
+            self.canvas.create_line(0,(i + 1) * (CANVAS_HIGHT / 8),CANVAS_WIDTH,(i + 1) * (CANVAS_HIGHT / 8))
+        self.canvas.pack()
+        self.idlist=[[0 for x in range(8)] for y in range(8)]
+        for i in range(8):
+            for j in range(8):
+                self.idlist[i][j]=self.canvas.create_oval(i*CANVAS_WIDTH/8+1, 1+j*CANVAS_HIGHT/8+1,
+                                                     (i+1)*CANVAS_WIDTH/8-1, (j+1)*CANVAS_HIGHT/8-1,
+                                                     fill='green',outline='green')
+        self.updateStone()
+    def setStone(self,x,y,color:int):
+        if color==0:
+            self.canvas.itemconfig(tagOrId=self.idlist[x][y],fill='green',outline='green')
+        elif color==1:
+            self.canvas.itemconfig(tagOrId=self.idlist[x][y], fill='black', outline='black')
+        elif color==2:
+            self.canvas.itemconfig(tagOrId=self.idlist[x][y], fill='white', outline='white')
+        self.canvas.update()
+    def updateStone(self):
+        for i in range(8):
+            for j in range(8):
+                self.setStone(i,j,self.env.state[i][j])
+
+
+if __name__ =="__main__":
+    root=tk.Tk()
+    environment=environment.Environment()
+    app=EnvGUI(master=root,env=environment)
+
+    app.mainloop()
---
+++ environment.py
@@ -0,0 +1,161 @@
+import tkinter as tk
+import numpy as np
+import envgui
+
+BLANK = 0  # 石が空：0
+BLACK = 1  # 石が黒：1
+WHITE = 2  # 石が白：2
+
+
+# 要件
+# 入力:行動([座標]or[])
+# 出力:最新の盤面(numpy)、行動ログ([[座標],手番]のリスト)、行動候補([[行動候補(座標リスト)]])、勝者(試合中は[])
+
+class Environment:
+    # 初期化
+    def __init__(self):
+        self.stateinit()
+        self.turn = BLACK
+        self.log = []
+        self.winner = []
+        self.isPassed = False
+
+    # stateの初期化
+    def stateinit(self):
+        self.state = np.zeros((8, 8))
+        self.state[3][3] = WHITE
+        self.state[4][3] = BLACK
+        self.state[3][4] = BLACK
+        self.state[4][4] = WHITE
+
+    # logの更新
+    def logupdate(self, act):
+        self.log.append([act, self.turn])
+
+    # logの出力
+    def getlog(self):
+        return np.copy(self.log)
+
+    # stateの出力
+    def getstate(self):
+        return np.copy(self.state)
+
+    # 勝者の出力
+    def getwinner(self):
+        return np.copy(self.winner)
+
+    def getturn(self):
+        return self.turn
+
+    # 手番の交代
+    def turn_change(self):
+        if self.turn == WHITE:
+            self.turn = BLACK
+        elif self.turn == BLACK:
+            self.turn = WHITE
+        else:
+            pass
+
+    # 取れる行動のリストを返す
+    def actlist(self):
+        actionlist = []
+        statecopy = np.copy(self.state)
+        for i in range(8):
+            for j in range(8):
+                if statecopy[i][j] == BLANK and self.reversestones([i, j]).size != 0:
+                    actionlist.append([i, j])
+        if len(actionlist) == 0: actionlist.append([])
+        return actionlist
+
+    # 行動をする。成功したらTrue、失敗ならFalseを返す
+    def action(self, act):
+        if  act in self.actlist():
+            if len(act) == 0:
+                if self.isPassed:
+                    self.decidewinner()
+                else:
+                    self.isPassed = True
+            else:
+                self.state = self.reversestones(act)
+                self.isPassed = False
+            self.logupdate(act)
+            self.turn_change()
+            return True
+        else:
+            return False
+
+    def decidewinner(self):
+        white = np.count_nonzero(self.state == WHITE)
+        black = np.count_nonzero(self.state == BLACK)
+        self.winner = max(white, black)
+
+    def reversestones(self, index):  # 引数に座標、出力は変化後の盤面orダメだったらfalse
+        if len(index) != 2 or self.state[index[0]][index[1]] != BLANK: return np.zeros(0)
+        arrayclone = np.copy(self.state)
+        for i in range(8):
+            localindex = np.array(index)
+            directon = [0, 0]
+            returnstonelist = []
+            if i == 0:
+                directon = [0, -1]
+            elif i == 1:
+                directon = [1, -1]
+            elif i == 2:
+                directon = [1, 0]
+            elif i == 3:
+                directon = [1, 1]
+            elif i == 4:
+                directon = [0, 1]
+            elif i == 5:
+                directon = [-1, 1]
+            elif i == 6:
+                directon = [-1, 0]
+            elif i == 7:
+                directon = [-1, -1]
+            isdifferentcoloredstone = False
+            localindex += directon  # 各方向にひとつづつ進める
+            while np.all(localindex >= 0) and np.all(localindex < 8):
+                if arrayclone[localindex[0]][localindex[1]] == BLANK: break
+                if arrayclone[localindex[0]][localindex[1]] != self.turn:  # 異なる色の石がある
+                    isdifferentcoloredstone = True
+                    returnstonelist.append(np.copy(localindex))
+                elif isdifferentcoloredstone:  # ↑の状態を経た後かつ同じ色の石がある ひっくり返せる
+                    for num in returnstonelist:
+                        arrayclone[num[0]][num[1]] = self.turn
+                    break
+                else:  # 上以外(同じ色しかない)
+                    break
+                localindex += directon  # 各方向にひとつづつ進める
+        if np.all(self.state == arrayclone):
+            return np.zeros(0)
+        else:
+            arrayclone[index[0]][index[1]] = self.turn
+            return arrayclone
+
+
+if __name__ == "__main__":
+    env = Environment()
+    while env.getwinner().size == 0:
+        t = env.getturn()
+        if t == WHITE:
+            print("turn of WHITE")
+        else:
+            print("turn of BLACK")
+        actlist = env.actlist()
+        act = False
+        while not act:
+            print(actlist)
+            print("which one? input number")
+            n = input()
+            if int(n) ==100:
+                root=tk.Tk()
+                envg=envgui.EnvGUI(env=env,master=root)
+                envg.mainloop()
+                continue
+            act = env.action(actlist[int(n)])
+    if env.getwinner()[0] == WHITE:
+        print("winner is WHITE")
+    else:
+        print("winner is  BLACK")
+
+
