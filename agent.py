b621bfb24910fb2affa17806e1a61156fa8ff039
7j2xgqhj <73765724+7j2xgqhj@users.noreply.github.com>
2023-07-19 15:41:46 +0900

    グラフとsoftmaxとεのsoftmaxの実装

--- agent.py
+++ agent.py
@@ -1,6 +1,7 @@
 import numpy as np
 import environment
 import random
+import matplotlib.pyplot as plt
 import math
 import time
 
@@ -8,28 +9,41 @@ BLANK = 0  # 石が空：0
 BLACK = 1  # 石が黒：1
 WHITE = -1  # 石が白：2
 
-SIZE=6
+SIZE = 4
+GAMMA = 0.9  # 割引率
+EPSILON = 0.8
+ATTENUATION = EPSILON / 80  # 減衰率
+TEMPERATURE =1#温度定数初期値    上げると等確率　下げると強調　加算減算ではなく比で考えて調整するのがいいかも
+EPTEMPERATURE=0.4
+
+WINREWORD=1
+LOSEREWORD=-1
+DRAWREWORD=-0.5
+
 class Agent:
     # 盤面の情報は、先手・後手(定数)、何手目(計算が簡単)、石値合計(np.sum(state))。打てる手数(ついでで使える)で分類して絞り込めるようにすることで計算時間を削減したい
     def __init__(self, side, dict):
         self.side = side
         self.tables = dict
         self.log = []
-        self.gamma = 0.9
-        self.epsilon = 0.8
+        self.gamma = GAMMA
+        self.epsilon = EPSILON
+        self.temperature=TEMPERATURE
         if side == BLACK:
             self.turn = 0
         elif side == WHITE:
             self.turn = 1
 
     def reset(self):
-        self.epsilon = 0.8
+        self.epsilon = EPSILON
         self.log = []
+        self.temperature = TEMPERATURE
         if self.side == BLACK:
             self.turn = 0
         elif self.side == WHITE:
             self.turn = 1
-    def action(self, state, actlist,islearn=None):##testとtrainをまとめたい　方策と記録のとこだけ変える
+
+    def action(self, state, actlist, islearn=None):  ##testとtrainをまとめたい　方策と記録のとこだけ変える
         if len(actlist) == 1:  # 選択肢が一つしかないとき
             act = actlist[0]
         else:
@@ -41,15 +55,15 @@ class Agent:
                 statesetlist = self.tables[key1][key2][key3]
                 statelist = [i[0] for i in statesetlist]
                 # テーブルにおいて、現在の盤面と一致する盤面が存在する場合そのindexを求める
-                indexl=[]
-                indexl = [i for i, x in enumerate(statelist) if np.all(x==state)]
+                indexl = []
+                indexl = [i for i, x in enumerate(statelist) if np.all(x == state)]
                 # 現在の盤面と一致する盤面が存在するとき
                 if len(indexl) == 1:
                     if islearn != None:
                         if islearn:
                             act = self.epsilongreedy(dict=statesetlist[indexl[0]][1], actlist=actlist)
                         else:
-                            policy = None  ##ここsoftmax入れたい
+                            act = self.softmaxchoice(dict=statesetlist[indexl[0]][1], actlist=actlist)
                     else:
                         act = self.maxreword(dict=statesetlist[indexl[0]][1], actlist=actlist)
                 else:
@@ -58,19 +72,30 @@ class Agent:
                 act = random.choice(actlist)
         if islearn:
             self.log.append([self.turn, actlist, act, state])
-            self.epsilon -= 0.01
+            self.epsilon -= ATTENUATION
         self.turn += 2
         return act
+
     def epsilongreedy(self, dict, actlist):  # dictは行動候補辞書{行動候補:[試行回数,行動価値]}
         if self.epsilon > random.random():
             set = [[a, dict[str(a)][1]] for a in actlist]
             act = random.choice([i[0] for i in set if i[1] == max([i[1] for i in set])])
         else:
-            act = random.choice(actlist)
+            vlist = [dict[str(a)][1] for a in actlist]
+            q = [np.exp(a / EPTEMPERATURE) for a in vlist]
+            plist = [qa / sum(q) for qa in q]
+            act = actlist[np.random.choice(list(range(len(actlist))), p=plist)]
         return act
+
     def maxreword(self, dict, actlist):
         set = [[a, dict[str(a)][1]] for a in actlist]
-        act=random.choice([i[0] for i in set if i[1] == max([i[1] for i in set])])
+        act = random.choice([i[0] for i in set if i[1] == max([i[1] for i in set])])
+        return act
+    def softmaxchoice(self, dict, actlist):
+        vlist = [dict[str(a)][1] for a in actlist]
+        q = [np.exp(a / self.temperature) for a in vlist]
+        plist=[qa/sum(q) for qa in q]
+        act = actlist[np.random.choice(list(range(len(actlist))), p=plist)]
         return act
     def save(self, reword):  # dict[ターン数][石値合計][選択肢の数]=[[state,{行動:[試行回数,行動価値] ...}],...]
         # step[0]:ターン数、step[1]:選択肢の配列、step[2]:選択した行動、step[3]:盤面(ndarray)
@@ -78,7 +103,7 @@ class Agent:
             # 割引現在価値
             r = reword * self.gamma ** (len(self.log) - (t + 1))
             key1 = str(step[0])
-            key2=str(np.sum(step[3]))
+            key2 = str(np.sum(step[3]))
             key3 = str(len(step[1]))
             key4 = str(step[2])
             state = step[3]
@@ -92,10 +117,10 @@ class Agent:
             # 絞り込んだ盤面の候補の中から一致する盤面のインデックスを返す
             statesetlist = self.tables[key1][key2][key3]
             statelist = [i[0] for i in statesetlist]
-            index = [i for i, x in enumerate(statelist) if np.all(x==state)]
+            index = [i for i, x in enumerate(statelist) if np.all(x == state)]
             if len(index) == 1:  # 一致する盤面が見つかった時
                 dict = self.tables[key1][key2][key3][index[0]][1]
-                if dict[key4][0]!=0:
+                if dict[key4][0] != 0:
                     dict[key4] += [1, (r - dict[key4][1]) / dict[key4][0]]
                 else:
                     dict[key4] += [1, r]
@@ -107,15 +132,16 @@ class Agent:
                     dict[str(a)] = np.array([0, 0], dtype=float)  # keyは"x,y" ここでnp配列で初期化している
                 dict[key4] += [1, r]  # 取った行動の記録
                 self.tables[key1][key2][key3].append([state, dict])
-            self.tables['count']+=1
+            self.tables['count'] += 1
 
     def savedict(self):
         if self.side == BLACK:
             np.save('tableblack.npy', self.tables)
         elif self.side == WHITE:
             np.save('tablewhite.npy', self.tables)
+
     def getfirststep(self):
-        dict=self.tables['2']
+        dict = self.tables['2']
         print(dict)
 
 
@@ -131,26 +157,26 @@ def train(episode):
             state = env.getstate()
             actlist = env.actlist
             if env.side == WHITE:
-                act = agentw.action(state=state, actlist=actlist,islearn=True)
+                act = agentw.action(state=state, actlist=actlist, islearn=True)
             else:
-                act = agentb.action(state=state, actlist=actlist,islearn=True)
-            #time.sleep(10)
+                act = agentb.action(state=state, actlist=actlist, islearn=True)
+            # time.sleep(10)
             env.action(act)
         winner = env.getwinner()
         if winner == WHITE:
-            agentw.save(1)
-            agentb.save(-1)
+            agentw.save(WINREWORD)
+            agentb.save(LOSEREWORD)
         elif winner == BLACK:
-            agentw.save(-1)
-            agentb.save(1)
+            agentw.save(LOSEREWORD)
+            agentb.save(WINREWORD)
         else:
-            agentw.save(-0.5)
-            agentb.save(-0.5)
+            agentw.save(DRAWREWORD)
+            agentb.save(DRAWREWORD)
         agentw.reset()
         agentb.reset()
     agentb.savedict()
     agentw.savedict()
-    #agentb.getfirststep()
+    # agentb.getfirststep()
 
 
 def test(whiteside, blackside, set):
@@ -158,12 +184,14 @@ def test(whiteside, blackside, set):
     dictb = np.load('tableblack.npy', allow_pickle=True).item()
     wwin = 0
     bwin = 0
+    draw = 0
     n = 0
     env = environment.Environment(SIZE)
     agentw = Agent(side=WHITE, dict=dictw)
     agentb = Agent(side=BLACK, dict=dictb)
+    scoreB = 0
+    scoreW = 0
     for count in range(set):
-
         env.reset()
         while env.getwinner().size == 0:
             actlist = env.actlist
@@ -182,12 +210,16 @@ def test(whiteside, blackside, set):
             wwin += 1
         elif winner == BLACK:
             bwin += 1
+        else:
+            draw += 1
         n += 1
         agentw.reset()
         agentb.reset()
-    print("Wwin" + str(wwin))
-    print("Bwin" + str(bwin))
-    print("総試合数" + str(n))
+    print("Wwin:" + str(wwin)+"回")
+    print("Bwin:" + str(bwin)+"回")
+    print("draw:"+str(draw)+"回")
+    print("総試合数:" + str(n)+"回")
+    return [wwin,bwin,draw,n]
 
 
 def resetW():
@@ -206,9 +238,22 @@ def dictcheckW():
 if __name__ == "__main__":
     resetB()
     resetW()
-    for i in range(5):
-        #t_s = time.perf_counter()
-        #train(1000)
-        #t_e = time.perf_counter()
-        #print(t_e - t_s)
-        test(whiteside=False,blackside=True,set=100)
+    num=0
+    x=[0]
+    y=[test(whiteside=False, blackside=True, set=1000)[1]]
+    for i in range(40):
+        train(100)
+        num+=100
+        x.append(num)
+        y.append(test(whiteside=False, blackside=True, set=1000)[1])
+    plt.plot(x, y)
+    plt.show()
+
+#    for i in range(5):
+#       cnum = num
+#      for j in range(9):
+#         print("追加" + str(cnum)+"回の訓練")
+#        train(cnum)
+#       num += cnum
+#      print("合計" + str(num)+"回の訓練")
+#     test(whiteside=False, blackside=True, set=100)
--- test.py
+++ test.py
@@ -1,5 +1,12 @@
 import numpy as np
-def softmax(l,t):
-       q=[np.exp(a/(1/t)) for a in l]
-       return [qa/sum(q) for qa in q]
-print(np.sum(np.array([[1,2],[1,2]])))
+
+def softmaxchoice(vlist, actlist):
+    print(actlist)
+    print(vlist)
+    vlist=[1/i for i in vlist]
+    q = [np.exp(a / 0.4) for a in vlist]
+    plist = [qa / sum(q) for qa in q]
+    print(plist)
+    act = actlist[np.random.choice(list(range(len(actlist))), p=plist)]
+    return act
+print(softmaxchoice([10,2,30],[[1,2],[3.4],[9,0]]))
